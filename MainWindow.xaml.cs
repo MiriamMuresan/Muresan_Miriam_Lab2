eaceaa50742ef366e0a89bc79de2fa4faf313fe8
Raluca Miriam Muresan <miriammuresan27@yahoo.ro>
2021-10-21 15:38:49 +0300

    Am implementat laboratorul 3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,13 @@ namespace Muresan_Miriam_Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PizzaMachine myPizzaMachine;
+
+        private int mMargheritaPizza; 
+        private int mPepperoniPizza; 
+        private int mVeggiePizza; 
+        private int mQuattroStagioniPizza; 
+        private int mCanibalePizza;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +35,93 @@ namespace Muresan_Miriam_Lab2
         private void cmbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void frm(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            myPizzaMachine = new PizzaMachine();
+            myPizzaMachine.PizzaComplete += new PizzaMachine.PizzaCompleteDelegate(PizzaCompleteHandler);
+        }
+
+        private void margPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            margPizzaMenuItem.IsChecked = true;
+            pepPizzaMenuItem.IsChecked = false;
+            vegPizzaMenuItem.IsChecked = false;
+            quatPizzaMenuItem.IsChecked = false;
+            canPizzaMenuItem.IsChecked = false;
+            myPizzaMachine.MakePizzas(PizzaType.Margherita);
+        }
+        private void pepPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            margPizzaMenuItem.IsChecked = false;
+            pepPizzaMenuItem.IsChecked = true;
+            vegPizzaMenuItem.IsChecked = false;
+            quatPizzaMenuItem.IsChecked = false;
+            canPizzaMenuItem.IsChecked = false;
+            myPizzaMachine.MakePizzas(PizzaType.Pepperoni);
+        }
+        private void vegPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            margPizzaMenuItem.IsChecked = false;
+            pepPizzaMenuItem.IsChecked = false;
+            vegPizzaMenuItem.IsChecked = true;
+            quatPizzaMenuItem.IsChecked = false;
+            canPizzaMenuItem.IsChecked = false;
+            myPizzaMachine.MakePizzas(PizzaType.Veggie);
+        }
+        private void quatPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // Completați codul sursă lipsă!
+        }
+        private void canPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // Completați codul sursă lipsă!
+        }
+
+        private void PizzaCompleteHandler()
+        {
+            switch (myPizzaMachine.Ingredients)
+            {
+                case PizzaType.Margherita:
+                    mMargheritaPizza++;
+                    txtMargheritaPizza.Text = mMargheritaPizza.ToString();
+                    break;
+                case PizzaType.Pepperoni:
+                    mPepperoniPizza++;
+                    txtPepperoniPizza.Text = mPepperoniPizza.ToString();
+                    break;
+                case PizzaType.Veggie:
+                    mVeggiePizza++;
+                    txtVeggiePizza.Text = mVeggiePizza.ToString();
+                    break;
+                    // Completați codul sursă lipsă!
+            }
+        }
+
+        private void stopMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myPizzaMachine.Enabled = false;
+        }
+
+        
+
+        private void txtQuantity_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!(e.Key >= Key.D0 && e.Key <= Key.D9))
+            {
+                MessageBox.Show("Numai cifre se pot introduce!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void exitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
